ec61959741224e7b92ba1c9e518281c92d552168
kirtikganesan <127873450+kirtikganesan@users.noreply.github.com>
2024-07-02 20:14:25 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import { Box } from "@chakra-ui/react";
 import EditorPage from "./components/EditorPage";
-import CodeEditor from "./components/CodeEditor";
 
 function App() {
   return (
     <Box minH="100vh" bg="#0f0a19" color="gray.500" px={6} py={8}>
       <EditorPage/>
-      <CodeEditor />
     </Box>
   );
 }
